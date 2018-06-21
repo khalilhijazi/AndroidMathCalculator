3502473295fe443fe6e974a7edde564a42f830c5
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-06-21 15:46:11 -0400

    Implemented division functionality

--- app/src/main/java/khalilbhijazi/gmail/com/mathcalculator/MainActivity.java
+++ app/src/main/java/khalilbhijazi/gmail/com/mathcalculator/MainActivity.java
@@ -58,5 +58,21 @@ public class MainActivity extends AppCompatActivity {
                 resultTextView.setText(num1 + " x " + num2 + " = " + product);
             }
         });
+
+        Button divideBtn = (Button) findViewById(R.id.divideBtn);
+        divideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                float num1 = Float.parseFloat(firstNumEditText.getText().toString());
+                float num2 = Float.parseFloat(secondNumEditText.getText().toString());
+                float quotient = num1 / num2;
+                resultTextView.setText(num1 + " / " + num2 + " = " + quotient);
+
+            }
+        });
     }
 }
